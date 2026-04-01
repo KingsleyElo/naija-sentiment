18b4e7e541b8fa6cc1cb3d42aea3635f7c3a4846
Kingsley Eloebhose <osazelekingsley0207@gmail.com>
2026-04-01 17:53:51 +0100

    updated utils.py script

--- 02-notebooks/utils.py
+++ 02-notebooks/utils.py
@@ -23,31 +23,6 @@ def dataset_preprocessing(text):
     return emoji_pattern.sub('', text).strip()
 
 
-import re
-import numpy as np
-import pickle
-
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-
-
-def dataset_preprocessing(text):
-    text = text.lower()
-    text = re.sub(r'http\S+', '', text)
-    emoji_pattern = re.compile(
-        "["
-        "\U0001F600-\U0001F64F"
-        "\U0001F300-\U0001F5FF"
-        "\U0001F680-\U0001F6FF"
-        "\U0001F1E0-\U0001F1FF"
-        "\U00002702-\U000027B0"
-        "\U000024C2-\U0001F251"
-        "]+",
-        flags=re.UNICODE
-    )
-    return emoji_pattern.sub('', text).strip()
-
-
 def preparation_for_model_train(X_train, X_val, X_test,
                                 y_train, y_val, y_test,
                                 vocab_size=50000, max_len=50,
